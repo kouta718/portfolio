2fc4e549aae7406b7fc85fee116046efe6894ae0
kouta718 <qooron1927@gmail.com>
2025-11-18 19:03:46 +0900

    コントローラーファイル作成 2025/11/18-pm7:03

---
+++ TooLexicon/app/Http/Controllers/ToolController.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ToolController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        // if ($post->locked) {
+        //     $request->session()->flash('message', 'ロックされています');
+        //     return redirect()->back();
+        // }
+    }
+
+    // ロック操作
+    public function lock()
+    {
+        // $post->locked = !$post->locked;
+        // $post->save();
+        // $message = $post->locked ? '投稿をロックしました。' : '投稿のロックを解除しました。';
+        // $request->session()->flash('message', $message);
+        // return redirect()->back();
+    }
+}
