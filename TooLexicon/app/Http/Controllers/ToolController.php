dd8850cef8d7db52070a48b7f1adca10ad173214
kouta718 <qooron1927@gmail.com>
2025-11-19 23:48:07 +0900

    コントローラーCRUD書き込み完了 2025/11/19-pm11:48

--- TooLexicon/app/Http/Controllers/ToolController.php
+++ TooLexicon/app/Http/Controllers/ToolController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Http\Requests\ToolStoreRequest;
+use App\Http\Requests\StoreToolRequest;
 use App\Models\Tool;
 
 class ToolController extends Controller
@@ -28,53 +28,51 @@ class ToolController extends Controller
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(StoreToolRequest $request)
     {
-        //
+        $validated = $request->validated();
+        Tool::create($validated);
+
+        return redirect()->route('tools.index')
+            ->with('success', '工具を登録しました');
     }
 
     /**
      * Display the specified resource.
      */
-    public function show(string $id)
+    public function show(Tool $tool)
     {
-        //
+        return view('tool.show', compact('tool'));
     }
 
     /**
      * Show the form for editing the specified resource.
      */
-    public function edit(string $id)
+    public function edit(Tool $tool)
     {
-        //
+        return view('tool.edit', compact('tool'));
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
+    public function update(StoreToolRequest $request, Tool $tool)
     {
-        //
+        $validated = $request->validated();
+        Tool::create($validated);
+
+        return redirect()->route('tools.show', $tool)
+            ->with('success', '工具のデータを更新しました');
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(string $id)
+    public function destroy(Tool $tool)
     {
-        // if ($post->locked) {
-        //     $request->session()->flash('message', 'ロックされています');
-        //     return redirect()->back();
-        // }
-    }
+        $tool->delete();
 
-    // ロック操作
-    public function lock()
-    {
-        // $post->locked = !$post->locked;
-        // $post->save();
-        // $message = $post->locked ? '投稿をロックしました。' : '投稿のロックを解除しました。';
-        // $request->session()->flash('message', $message);
-        // return redirect()->back();
+    return redirect()->route('tools.index')
+        ->with('success', '削除しました');
     }
 }
