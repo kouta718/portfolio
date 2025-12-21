40754c9a65b53dad75830c28e0fd6e735ccf550f
KUKUKUKU718 <qooron1927@gmail.com>
2025-12-21 23:42:24 +0900

    2025/12/21-pm12:42

---
+++ TooLexicon/resources/views/components/svg.blade.php
@@ -0,0 +1,20 @@
+{{-- コンポーネントが受け取る 引数 --}}
+@props([
+    'name',
+    'class' => 'w-5 h-5',
+])
+
+{{-- SVGファイルのパス --}}
+@php
+    $path = public_path("icons/{$name}.svg");
+@endphp
+
+@if (file_exists($path))
+    {!! str_replace(
+        '<svg',
+        '<svg '.$attributes->merge(['class' => $class]),
+        file_get_contents($path)
+    ) !!}
+@else
+    <!-- icon not found: {{ $name }} -->
+@endif
